425071825ebc35efb32cbdb4ace873a61579bb6d
Oliver Hemsley <ccenturionnmc@gmail.com>
2021-11-22 13:26:29 +0000

    Basic keystroke logging

---
+++ capture_keystrokes.py
@@ -0,0 +1,40 @@
+from pynput import keyboard
+import numpy as np
+import pandas as pd
+import time
+
+start = 0
+end = 0
+keystroke_array = []
+count = 0
+
+def on_press(key):
+    global start 
+    start = time.perf_counter_ns()
+    start /= 1000000
+    try:
+        print(f'alphanumeric key {key.char} pressed')
+    except AttributeError:
+        print(f'special key {key} pressed')
+
+def on_release(key):
+    global end, start, keystroke_array, count
+    end = time.perf_counter_ns()
+    end /= 1000000
+    print(f'{key} released after {end - start} milliseconds')
+
+    print(keystroke_array)
+    keystroke_array.append([key.char, start, end])
+
+    print(keystroke_array)
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+# listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+# listener.start()
