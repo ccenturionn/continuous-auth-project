e84bc59a7af281b848837f12a88deb9293830af5
Oliver Hemsley <ccenturionnmc@gmail.com>
2022-03-10 11:46:47 +0000

    Additional testing

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pickle
 import pandas as pd
 import analyse_keystrokes
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
@@ -14,7 +13,7 @@ def add_user(username):
     for i in range(reps):
         keystroke_array = capture_keystrokes.record_keystrokes()
 
-        print(keystroke_array)
+        # print(keystroke_array)
 
         with open(username + "_keystroke_data_" + str(i+1), 'wb') as file:
             pickle.dump(keystroke_array, file)
@@ -38,7 +37,7 @@ def add_user(username):
 
     file.close()
 
-# add_user("lauren")
+# add_user("test")
 
 # with open("oliver_keystroke_features", 'rb') as file:
 #             print(pickle.load(file))
@@ -70,18 +69,21 @@ index = keystroke_features.index
 # tfidf_vectorizer = TfidfVectorizer(use_idf=True)
 # x_vectors = tfidf_vectorizer.fit_transform(x)
 
-x_train, x_test, y_train, y_test, index_train, index_test = train_test_split(x, y, index, train_size=0.6, shuffle=True)
+# x_train, x_test, y_train, y_test, index_train, index_test = train_test_split(x, y, index, train_size=0.6, shuffle=True)
+x_train, x_test, y_train, y_test, index_train, index_test = train_test_split(x, y, index, train_size=0.9, shuffle=True)
 
 y_train = y_train.astype('int')
 y_test = y_test.astype('int')
 
+print("Training ML Classifier...")
+
 logistic_reg = LogisticRegression(random_state=0)
 logistic_reg.fit(x_train, y_train)
 
-lr_pred = logistic_reg.predict(x_test)
+# lr_pred = logistic_reg.predict(x_test)
 
 # print(classification_report(y_test, lr_pred))
-print(lr_pred)
+# print(lr_pred)
 
 
 
@@ -113,4 +115,7 @@ test_keystroke_features = analyse_keystrokes.calc_features(keystroke_array)
 
 lr_pred = logistic_reg.predict(test_keystroke_features.head(1))
 
-print(lr_pred)
+if lr_pred[0] == 1:
+    print("Current user: Oliver")
+else:
+    print("Current user: Lauren")
