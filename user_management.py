7f928b4f40189618f7436ec6746c173bc944bb6c
Oliver Hemsley <ccenturionnmc@gmail.com>
2022-04-05 17:15:26 +0100

    Added comments

--- user_management.py
+++ user_management.py
@@ -8,12 +8,14 @@ import analyse_keystrokes
 import capture_keystrokes
 import classify
 
+# Check if user_num_store file exists if not create empty dict
 if exists("user_data\\user_num_store") == False:
     user_num = {}
     with open("user_data\\user_num_store", 'wb') as file:
         pickle.dump(user_num, file)
     file.close()
 
+# Check if keystroke_features_store file exists if not create empty datafram
 if exists("user_data\\keystroke_features_store") == False:
     print("Please type random letters to initialise keystroke dataframe.")
     analyse_keystrokes.create_empty_df(capture_keystrokes.record_keystrokes())
@@ -26,9 +28,11 @@ def add_user(username="none_provided", reps=0):
     Adds a user to the system
     """
 
+    # Check if username is provided if not, prompt user to enter
     if username == "none_provided":
         username = input("Enter a username to add: ")
 
+    # Prompt for number of training repetitions
     if reps == 0:
         reps = int(input("Enter the number of training repetitions the user should complete: "))
 
@@ -58,10 +62,12 @@ def add_user(username="none_provided", reps=0):
 
     keystroke_features['user'] = num
 
+    # Load master_keystroke_features from file
     with open("user_data\\keystroke_features_store", 'rb') as file:
         master_keystroke_features = pickle.load(file)
     file.close()
 
+    # Concat the new keystroke features to master_keystroke_features
     master_keystroke_features = pd.concat([master_keystroke_features, keystroke_features])
 
     # Store the features dataframe to file
@@ -74,6 +80,7 @@ def add_user(username="none_provided", reps=0):
         pickle.dump(user_num, file)
     file.close()
 
+    # Check if there is more that one user before training classifier
     if len(user_num) > 1:
         classify.train_classifier()
 
@@ -87,19 +94,23 @@ def remove_user(username="none_provided"):
     Removes a user from the system
     """
 
+    # Check if username is provided, if not prompt user to enter username
     if username == "none_provided":
         username = input("Enter a username to remove: ")
 
+    # Load user_num_store from file
     with open("user_data\\user_num_store", 'rb') as file:
         user_num = pickle.load(file)
     file.close()
 
+    # Get user number for the username provided
     num = list(user_num.keys())[list(user_num.values()).index(username)]
 
     with open("user_data\\keystroke_features_store", 'rb') as file:
         master_keystroke_features = pickle.load(file)
     file.close()
 
+    # Remove user from master_keystroke_features
     master_keystroke_features = master_keystroke_features[master_keystroke_features['user'] != num]
 
     # Store the features dataframe to file
@@ -109,8 +120,10 @@ def remove_user(username="none_provided"):
 
     print(f"Successfully removed {user_num[num]} (user no. {num}) from the keystroke feature datastore.")
 
+    # Remove user from user_num dict
     user_num.pop(num, None)
 
+    # Write user_num_store to file
     with open("user_data\\user_num_store", 'wb') as file:
         pickle.dump(user_num, file)
     file.close()
@@ -145,6 +158,9 @@ def print_keystroke_datastore():
     input("Press enter to continue...")
 
 def manage_users():
+    """
+    Main menu for managing users
+    """
     
     dispatcher = {'1': add_user, '2': remove_user, '3': list_users, '4': print_keystroke_datastore}
     
